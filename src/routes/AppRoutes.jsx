49191231df8c94fd645fffdcb33a1a77c9901ff8
lenuel123 <lenuel.cutor@gmail.com>
2025-03-13 12:11:16 +0800

    update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { Suspense, lazy } from "react";
 
 const Main = lazy(() => import("../pages/Main"));
-// const loading = () => <div>Loading...</div>;
+
 const AppRoutes = () => {
     return (
-        <Router>
-            <Suspense>
+        <Router basename="/my-website">
+            <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path="/" element={<Main />} />
                 </Routes>
             </Suspense>
         </Router>
     );
-};  
+};
 
 export default AppRoutes;
